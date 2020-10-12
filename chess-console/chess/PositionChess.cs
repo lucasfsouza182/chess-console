a309ccf1fea27dba769d614a077636f70e07c598
Lucas Ferreira <lucasfsouza182@gmail.com>
2020-10-12 19:55:53 -0300

    feat: create chess position class

---
+++ chess-console/chess/PositionChess.cs
@@ -0,0 +1,29 @@
+﻿using board;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chess
+{
+    class PositionChess
+    {
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+        public PositionChess(char column, int line)
+        {
+            Column = column;
+            Line = line;
+        }
+
+        public Position ToBoardPosition()
+        {
+            return new Position(8 - Line, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Line;
+        }
+    }
+}
